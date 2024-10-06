1915340aaf81d7ec495baaacc6e24e8111165a5c
KulikovNikita97 <sergei3141@mail.ru>
2024-10-06 14:56:51 +0500

    video chunked

--- src/App.js
+++ src/App.js
@@ -1,132 +1,117 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
+import ReactPlayer from 'react-player';
+import axios from 'axios';
 
-const VideoRecorder = () => {
-  const videoLiveRef = useRef(null);
-  const videoRecordedRef = useRef(null);
-  const buttonStartRef = useRef(null);
-  const buttonStopRef = useRef(null);
-  const buttonSendRef = useRef(null);
-  const [recordedBlobs, setRecordedBlobs] = useState([]);
+function VideoRecorder() {
+  const [recordedBlob, setRecordedBlob] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const videoRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
 
-  useEffect(() => {
-    const videoLive = videoLiveRef.current;
-    const videoRecorded = videoRecordedRef.current;
-    const buttonStart = buttonStartRef.current;
-    const buttonStop = buttonStopRef.current;
-    const buttonSend = buttonSendRef.current;
-
-    const startRecording = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
-
-      videoLive.srcObject = stream;
-
-      if (!MediaRecorder.isTypeSupported('video/webm')) {
-        console.warn('video/webm is not supported');
-        return;
-      }
-
-      const mediaRecorder = new MediaRecorder(stream, {
-        mimeType: 'video/webm',
-      });
-
-      let blobs = [];
-
-      buttonStart.addEventListener('click', () => {
-        mediaRecorder.start();
-        buttonStart.setAttribute('disabled', '');
-        buttonStop.removeAttribute('disabled');
-        buttonSend.setAttribute('disabled', ''); // Отключаем кнопку отправки во время записи
-        blobs = []; // Очищаем массив blobs при начале новой записи
-        setRecordedBlobs([]);
-      });
-
-      buttonStop.addEventListener('click', () => {
-        mediaRecorder.stop();
-        buttonStart.removeAttribute('disabled');
-        buttonStop.setAttribute('disabled', '');
-        buttonSend.removeAttribute('disabled'); // Включаем кнопку отправки после остановки записи
-      });
-
-      mediaRecorder.addEventListener('dataavailable', (event) => {
-        blobs.push(event.data);
-      });
-
-      mediaRecorder.addEventListener('stop', () => {
-        const videoBlob = new Blob(blobs, { type: 'video/webm' });
-        setRecordedBlobs([videoBlob]); // Сохраняем blob в состоянии
-        videoRecorded.src = URL.createObjectURL(videoBlob);
-      });
+  // ЗАПИСЬ ВИДЕО
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    videoRef.current.srcObject = stream;
+    
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorderRef.current = mediaRecorder;
+    
+    mediaRecorder.ondataavailable = (event) => {
+      setRecordedBlob(event.data);
+      mediaRecorder.stop();
     };
+    
+    mediaRecorder.start();
+    setIsRecording(true);
+  };
 
-    startRecording();
-
-    return () => {
-      const stream = videoLive.srcObject;
-      if (stream) {
-        stream.getTracks().forEach(track => track.stop());
-        videoLive.srcObject = null;
-      }
-    };
-  }, []);
-
-  // ОТПРАВКА НА СЕРВЕР
-  const handleSendVideo = async () => {
-    if (recordedBlobs.length === 0) {
+  // ОТПРАВКА НА СЕРВЕР 
+  const handleUpload = () => {
+    if (recordedBlob) {
+      uploadFile(recordedBlob);
+    } else {
       console.error('Нет записанного видео для отправки');
-      return;
-    }
-debugger
-    const formData = new FormData();
-    formData.append('video', recordedBlobs[0], 'recorded_video.webm');
-
-    try {
-      const response = await fetch('https://your-backend-server.com/upload', { 
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        console.log('Видео успешно отправлено!');
-        // Дополнительные действия после успешной отправки
-      } else {
-        console.error('Ошибка при отправке видео:', response.status);
-        // Обработка ошибок отправки
-      }
-    } catch (error) {
-      console.error('Произошла ошибка:', error);
-      // Обработка ошибок сети или других непредвиденных ошибок
     }
   };
 
+  const uploadFile = async (file) => {
+
+    const chunkSize = 1024 * 10; // 10 Кб
+    const formData = new FormData();
+
+    formData.append('file_name', 'file.name');
+    formData.append('file_size', file.size);
+    formData.append('mime_type', 'string');
+    formData.append('user', 1);
+    formData.append('group_id', 421);
+    formData.append('total_chunks', Math.ceil(file.size/(chunkSize)));
+
+    try {
+      // Инициируем загрузку файла (отправляем рамер, имя и получаем id для загрузки чанков)
+      const response = await axios.post('http://127.0.0.1:8000/api/video/upload/init/', formData);
+      const uploadId = response.data.upload_id;
+
+      // Загрузка чанка (кол-во циклов зависит от кол-ва чанков в файле)
+      for (let offset = 0; offset < file.size; offset += chunkSize) {
+
+        const reader = new FileReader();
+
+        await new Promise((resolve) => {
+          reader.onload = async (event) => {
+
+            // Вычисляем индексы и кол-во чанков
+            const chunkBlob = new Blob([event.target.result], { type: 'video/mp4' });
+            const chunkIndex = Math.floor(offset / chunkSize);
+
+            const formData = new FormData();
+    
+            formData.append('chunk_index', chunkIndex);
+            formData.append('chunk', chunkBlob, `chunk_${chunkIndex}`);
+    
+            try {
+              await axios.patch(`http://127.0.0.1:8000/api/video/upload/${uploadId}/chunk/`, formData);
+              console.log(`Чанк ${chunkIndex} отправлен`);
+            } catch (error) {
+              console.error('Ошибка при отправке чанка:', error);
+            }
+            resolve();
+          };
+          // Читаем текущий чанк файла (предыдущий размер + 10 Кб)
+          reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize));
+        });
+      }
+      const completeResponse = await axios.post(`http://127.0.0.1:8000/api/video/upload/${uploadId}/complete/`);
+      console.log(`Загрузка файла завершена, статус: ${completeResponse.data.status}, URL: ${completeResponse.data.file_url}`);
+    } catch (error) {
+      console.error('Ошибка при отправке информации о файле:', error);
+    }
+  };
+
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    setIsRecording(false);
+  };
+
   return (
-    <>
-      <div>
-        <button type="button" id="buttonStart" ref={buttonStartRef}>
-          Start
-        </button>
-        <button type="button" id="buttonStop" disabled ref={buttonStopRef}>
-          Stop
-        </button>
-        <button
-          type="button"
-          id="buttonSend"
-          ref={buttonSendRef}
-          onClick={handleSendVideo}
-        >
-          Send
-        </button>
-      </div>
-      <div>
-        <video autoPlay muted playsInline id="videoLive" ref={videoLiveRef}></video>
-      </div>
-      <div>
-        <video controls playsInline id="videoRecorded" ref={videoRecordedRef}></video>
-      </div>
-    </>
+    <div>
+      <video ref={videoRef} autoPlay muted />
+      <button onClick={startRecording} disabled={isRecording}>
+        Начать запись
+      </button>
+      <button onClick={stopRecording} disabled={!isRecording}>
+        Остановить запись
+      </button>
+      {recordedBlob && (
+        <ReactPlayer
+          url={URL.createObjectURL(recordedBlob)}
+          controls
+          width="100%"
+          height="300px"
+        />
+      )}
+      <button onClick={handleUpload} disabled={!recordedBlob}>Отправить видео</button>
+    </div>
   );
-};
+}
 
 export default VideoRecorder;
